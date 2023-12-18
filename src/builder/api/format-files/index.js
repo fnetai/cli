90d456bd019f3b3b0dc080b214f6e9731a52ed82
gboyraz <gultekinboyraz@gmail.com>
2023-12-18 14:50:58 +0100

    wip

--- src/builder/api/format-files/index.js
+++ src/builder/api/format-files/index.js
@@ -8,8 +8,7 @@ module.exports = async ({ setInProgress, context }) => {
     await setInProgress({ message: "Prettifiying source files." });
 
     let srcDir=path.join("src","**","*");
-    let binDir=path.join("src","**","*");
 
-    const result = shell.exec(`prettier --write ${srcDir} ${binDir} *.{js,json,yaml,html} --no-error-on-unmatched-pattern`, { cwd: projectDir });
+    const result = shell.exec(`prettier --write ${srcDir} *.{js,json,yaml,html} --no-error-on-unmatched-pattern`, { cwd: projectDir });
     if (result.code !== 0) throw new Error('Couldnt format files.');
 }
