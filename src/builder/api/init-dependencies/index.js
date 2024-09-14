6d1c7e2cfbace9c526f111b789c1a4e106caee94
gboyraz <gultekinboyraz@gmail.com>
2024-09-14 15:18:37 +0200

    chore: Update package-lock.json and package.json to version 0.38.1

--- src/builder/api/init-dependencies/index.js
+++ src/builder/api/init-dependencies/index.js
@@ -3,8 +3,8 @@ const fnetListNpmVersions = require('@flownet/lib-list-npm-versions');
 module.exports = async ({ atom, packageDependencies, packageDevDependencies }) => {
 
   if (atom.type === 'workflow') {
-    packageDependencies.push({ package: "get-value", version: "^3.0" });
-    packageDependencies.push({ package: "set-value", version: "^4.1" });
+    packageDependencies.push({ package: "get-value", version: "^3" });
+    packageDependencies.push({ package: "set-value", version: "^4" });
   }
 
   if (atom.doc.features.form_enabled) {
@@ -24,7 +24,7 @@ module.exports = async ({ atom, packageDependencies, packageDevDependencies }) =
   }
 
   if (atom.doc.features.preact_enabled) {
-    packageDependencies.push({ package: "preact", version: "^10.19" });
+    packageDependencies.push({ package: "preact", version: "^10" });
   }
 
   if (atom.doc.features.cli.enabled === true) {
@@ -34,30 +34,30 @@ module.exports = async ({ atom, packageDependencies, packageDevDependencies }) =
   // packageDependencies.push({ package: "chalk", version: "^4" });
 
   // DEV DEPENDENCIES
-  packageDevDependencies.push({ package: "@babel/core", version: "^7.23" });
-  packageDevDependencies.push({ package: "@rollup/plugin-commonjs", version: "^26.0" });
-  packageDevDependencies.push({ package: "@rollup/plugin-node-resolve", version: "^15.1" });
-  packageDevDependencies.push({ package: "@rollup/plugin-replace", version: "^5.0" });
-  packageDevDependencies.push({ package: "rollup", version: "^4.9" });
-  packageDevDependencies.push({ package: "rollup-plugin-delete", version: "^2.0" });
-  packageDevDependencies.push({ package: "rollup-plugin-dts", version: "^6.0" });
-  packageDevDependencies.push({ package: "rollup-plugin-peer-deps-external", version: "^2.2" });
-  packageDevDependencies.push({ package: "@rollup/plugin-alias", version: "^5.0" });
+  packageDevDependencies.push({ package: "@babel/core", version: "^7" });
+  packageDevDependencies.push({ package: "@rollup/plugin-commonjs", version: "^26" });
+  packageDevDependencies.push({ package: "@rollup/plugin-node-resolve", version: "^15" });
+  packageDevDependencies.push({ package: "@rollup/plugin-replace", version: "^5" });
+  packageDevDependencies.push({ package: "rollup", version: "^4" });
+  packageDevDependencies.push({ package: "rollup-plugin-delete", version: "^2" });
+  packageDevDependencies.push({ package: "rollup-plugin-dts", version: "^6" });
+  packageDevDependencies.push({ package: "rollup-plugin-peer-deps-external", version: "^2" });
+  packageDevDependencies.push({ package: "@rollup/plugin-alias", version: "^5" });
   // packageDevDependencies.push({ package: "@rollup/plugin-esm-shim", version: "^0.1" });
-  packageDevDependencies.push({ package: "fs-extra", version: "^11.2" });
+  packageDevDependencies.push({ package: "fs-extra", version: "^11" });
 
   if (atom.doc.features.babel_enabled) {
-    packageDevDependencies.push({ package: "@rollup/plugin-babel", version: "^6.0" });
-    packageDevDependencies.push({ package: "@babel/preset-env", version: "7.24" });
-    packageDevDependencies.push({ package: "@babel/preset-react", version: "7.24" });
+    packageDevDependencies.push({ package: "@rollup/plugin-babel", version: "^6" });
+    packageDevDependencies.push({ package: "@babel/preset-env", version: "^7" });
+    packageDevDependencies.push({ package: "@babel/preset-react", version: "^7" });
   }
 
   if (atom.doc.features.browsersync_enabled) {
-    packageDevDependencies.push({ package: "rollup-plugin-browsersync", version: "^1.3" });
+    packageDevDependencies.push({ package: "rollup-plugin-browsersync", version: "^1" });
   }
 
   if (atom.doc.features.json_enabled) {
-    packageDevDependencies.push({ package: "@rollup/plugin-json", version: "^6.0" });
+    packageDevDependencies.push({ package: "@rollup/plugin-json", version: "^6" });
   }
 
   if (atom.doc.features.terser_enabled) {
@@ -65,53 +65,53 @@ module.exports = async ({ atom, packageDependencies, packageDevDependencies }) =
   }
 
   if (atom.doc.features.wasm_enabled) {
-    packageDevDependencies.push({ package: "@rollup/plugin-wasm", version: "^6.1" });
+    packageDevDependencies.push({ package: "@rollup/plugin-wasm", version: "^6" });
   }
 
   if (atom.doc.features.image_enabled) {
-    packageDevDependencies.push({ package: "@rollup/plugin-image", version: "^3.0" });
+    packageDevDependencies.push({ package: "@rollup/plugin-image", version: "^3" });
   }
 
   if (atom.doc.features.analyzer_enabled) {
-    packageDevDependencies.push({ package: "rollup-plugin-analyzer", version: "^3.3" });
+    packageDevDependencies.push({ package: "rollup-plugin-analyzer", version: "^3" });
   }
 
   if (atom.doc.features.visualizer_enabled) {
-    packageDevDependencies.push({ package: "rollup-plugin-visualizer", version: "^5.9" });
+    packageDevDependencies.push({ package: "rollup-plugin-visualizer", version: "^5" });
   }
 
   if (atom.doc.features.string_enabled) {
-    packageDevDependencies.push({ package: "rollup-plugin-string", version: "^3.0" });
+    packageDevDependencies.push({ package: "rollup-plugin-string", version: "^3" });
   }
 
   if (atom.doc.features.css_enabled) {
-    packageDevDependencies.push({ package: "rollup-plugin-postcss", version: "4.0" });
+    packageDevDependencies.push({ package: "rollup-plugin-postcss", version: "^4" });
     packageDevDependencies.push({ package: "sass", version: "^1.66" });
 
     const plugins = atom.doc.features.css_options?.plugins || [];
     plugins.forEach(plugin => {
       switch (plugin.name) {
         case 'postcss-import':
-          packageDevDependencies.push({ package: "postcss-import", version: "^15.1" });
+          packageDevDependencies.push({ package: "postcss-import", version: "^15" });
           break;
         case 'postcss-url':
-          packageDevDependencies.push({ package: "postcss-url", version: "^10.1" });
+          packageDevDependencies.push({ package: "postcss-url", version: "^10" });
           break;
         case 'postcss-preset-env':
-          packageDevDependencies.push({ package: "postcss-preset-env", version: "^9.1" });
+          packageDevDependencies.push({ package: "postcss-preset-env", version: "^9" });
           break;
         case 'autoprefixer':
-          packageDevDependencies.push({ package: "autoprefixer", version: "^10.4" });
+          packageDevDependencies.push({ package: "autoprefixer", version: "^10" });
           break;
         case 'cssnano':
-          packageDevDependencies.push({ package: "cssnano", version: "^6.0" });
+          packageDevDependencies.push({ package: "cssnano", version: "^6" });
           break;
       }
     });
   }
 
   if (atom.doc.features.copy_enabled) {
-    packageDevDependencies.push({ package: "rollup-plugin-copy", version: "^3.5" });
-    packageDevDependencies.push({ package: "chokidar", version: "^3.6" });
+    packageDevDependencies.push({ package: "rollup-plugin-copy", version: "^3" });
+    packageDevDependencies.push({ package: "chokidar", version: "^3" });
   }
 }
