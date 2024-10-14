99d0579bd935581007ebf46e5c24b4a2dbfc9c72
Gultekin Boyraz <g.boyraz@azerion.com>
2024-10-14 18:04:22 +0200

    Bump package version to 0.64.4; update validation schema handling in create-to-yargs/index.js; modify package-lock.json and package.json

--- src/builder/api/create-to-yargs/index.js
+++ src/builder/api/create-to-yargs/index.js
@@ -1,8 +1,6 @@
 const fs = require("node:fs");
 const path = require("node:path");
 const nunjucks = require("nunjucks");
-// const fnetYargsOptionsFromSchema = require("@fnet/yargs-options-from-schema");
-// const fnetYaml = require("@fnet/yaml");
 const Ajv = require('ajv/dist/2020');
 const standaloneCode = require('ajv/dist/standalone');
 
@@ -12,46 +10,20 @@ module.exports = async ({ atom, setInProgress, context, njEnv }) => {
 
   await setInProgress({ message: "Creating yargs." });
 
-  let options = {};
-  // const inputs = atom.doc.inputs || [];
+  let schema = {};
   const imports = [];
 
   const input = atom.doc.input;
   if (input) {
-    // options = await fnetYargsOptionsFromSchema({ schema: input });
-    options = atom.doc.input;
+    schema = atom.doc.input;
   }
   else {
-    // define basic schema
-    options = {
+    schema = {
       type: "object",
       properties: {},
       required: []
     };
   };
-
-  // else
-  //   inputs.forEach(input => {
-  //     if (input.cli === false || !input.name) return;
-  //     if (input.import) imports.push(input);
-
-  //     const option = {};
-
-  //     if (Reflect.has(input, 'type')) option.type = input.type;
-  //     if (Reflect.has(input, 'default')) option.default = input.default;
-  //     if (Reflect.has(input, 'choices')) option.choices = input.choices;
-  //     if (Reflect.has(input, 'describe') || Reflect.has(input, 'description')) {
-  //       option.describe = input.describe || input.description;
-  //     }
-  //     if (Reflect.has(input, 'alias')) option.alias = input.alias;
-  //     if (Reflect.has(input, 'required') && input.required === true) option.required = true;
-  //     if (Reflect.has(input, 'hidden') && input.hidden === true) option.hidden = true;
-  //     if (Reflect.has(input, 'array') && input.array === true) option.array = true;
-  //     if (Reflect.has(input, 'normalize') && input.normalize === true) option.normalize = true;
-  //     if (Reflect.has(input, 'nargs')) option.nargs = input.nargs;
-
-  //     options[input.name] = option;
-  //   });
 
   if (atom.doc.features.cli.fargs && atom.doc.features.cli.fargs?.enabled !== false) {
 
@@ -64,13 +36,13 @@ module.exports = async ({ atom, setInProgress, context, njEnv }) => {
     // if (Reflect.has(fargsOptions, 'describe') || Reflect.has(fargsOptions, 'description')) fargs.describe = fargsOptions.describe || fargsOptions.description;
     // if (Reflect.has(fargsOptions, 'choices')) fargs.choices = fargsOptions.choices;
 
-    if (options.properties) {
-      options.properties["fargs"] = fargs;
-      options.properties["ftag"] = ftag;
+    if (schema.properties) {
+      schema.properties["fargs"] = fargs;
+      schema.properties["ftag"] = ftag;
     }
   }
 
-  const templateContext = { options, imports, atom: atom }
+  const templateContext = { options: schema, imports, atom: atom }
 
   const templateDir = context.templateDir;
   const template = nunjucks.compile(
@@ -97,7 +69,7 @@ module.exports = async ({ atom, setInProgress, context, njEnv }) => {
     },
   });
 
-  const validate = ajv.compile(atom.doc.input);
+  const validate = ajv.compile(schema);
   const validateCode = standaloneCode(ajv, validate);
   
   fs.writeFileSync(path.resolve(projectDir, `src/default/validate_input.js`), validateCode, 'utf8');
