5f4439469271315e50f87b9963ea83b6e349c3db
gboyraz <gultekinboyraz@gmail.com>
2024-11-07 23:49:23 +0100

    Bump package version to 0.69.1; update formatting logic to use fnetShellFlow and clean up code

--- src/builder/api/format-files/index.js
+++ src/builder/api/format-files/index.js
@@ -1,14 +1,22 @@
-const shell = require('shelljs');
-const path=require("path");
+// const shell = require('shelljs');
+const path = require("path");
+const fnetShellFlow = require("@fnet/shell-flow");
 
 module.exports = async ({ setProgress, context }) => {
 
-    const projectDir = context.projectDir;
+  const projectDir = context.projectDir;
 
-    await setProgress({ message: "Prettifiying source files." });
+  await setProgress({ message: "Prettifiying source files." });
 
-    let srcDir=path.join("src","**","*");
+  let srcDir = path.join("src", "**", "*");
 
-    const result = shell.exec(`prettier --write ${srcDir} *.{js,cjs,mjs,json,yaml,html} --no-error-on-unmatched-pattern`, { cwd: projectDir });
-    if (result.code !== 0) throw new Error('Couldnt format files.');
+  await fnetShellFlow({
+    commands: {
+      steps: [`prettier --write ${srcDir} *.{js,cjs,mjs,json,yaml,html} --no-error-on-unmatched-pattern`],
+      wdir: projectDir,
+    }
+  });
+  
+  // const result = shell.exec(`prettier --write ${srcDir} *.{js,cjs,mjs,json,yaml,html} --no-error-on-unmatched-pattern`, { cwd: projectDir });
+  // if (result.code !== 0) throw new Error('Couldnt format files.');
 }
