306d40829133be319b6dab378ec31fb2994dc4c7
gboyraz <gultekinboyraz@gmail.com>
2025-01-10 23:08:39 +0100

    Bump version to 0.89.0 in package.json and package-lock.json; update @fnet/cli-project-common dependency to 0.3.2 and @fnet/cli-project-flow dependency to 0.8.2; enhance init-modules logic to handle non-array modules

--- src/builder/block/common/init-modules.js
+++ src/builder/block/common/init-modules.js
@@ -1,7 +1,27 @@
 module.exports = async ({ node, initNode }) => {
 
+  if (Reflect.has(node.definition, 'modules') && !Array.isArray(node.definition.modules)) {
+    const modules = node.definition.modules;
+
+    node.definition.modules = [];
+
+    Object.keys(modules).forEach(key => {
+      const transformed = {
+        ...modules[key]
+      }
+
+      if (node.type === 'modules') {
+        transformed.export = transformed.export || `mod.${key}`;
+      }
+
+      node.definition.modules.push({
+        [key]: transformed
+      });
+    });
+  }
+
   node.hasModules = node.definition.modules?.length > 0;
-  
+
   for (let i = 0; i < node.definition.modules?.length; i++) {
     const temp = node.definition.modules[i];
     const key = Object.keys(temp)[0];
--- src/builder/block/modules/index.js
+++ src/builder/block/modules/index.js
@@ -1,38 +1,38 @@
 const cloneDeep = require('lodash.clonedeep');
-const initModules= require('../common/init-modules');
+const initModules = require('../common/init-modules');
 
 async function hits({ node }) {
-    return node.definition.hasOwnProperty('modules');
+  return node.definition.hasOwnProperty('modules');
 }
 
 async function init({ node, initNode }) {
-    node.type = "modules"
+  node.type = "modules"
 
-    await initModules({ node, initNode });
-        
-    node.resolve = resolve;
+  await initModules({ node, initNode });
+
+  node.resolve = resolve;
 }
 
 async function resolve({ node, resolveTypeCommon, resolveNextBlock, transformExpression }) {
 
-    node.context.transform = node.context.transform || cloneDeep(node.definition);
-    const transform = node.context.transform;
+  node.context.transform = node.context.transform || cloneDeep(node.definition);
+  const transform = node.context.transform;
 
-    if (transform.export)
-      transform.export = await transformExpression(transform.export);
+  if (transform.export)
+    transform.export = await transformExpression(transform.export);
 
-    if (Reflect.has(transform, 'return')){
-      node.returns = true;
-      transform.return = await transformExpression(transform.return);
-    } 
+  if (Reflect.has(transform, 'return')) {
+    node.returns = true;
+    transform.return = await transformExpression(transform.return);
+  }
+  
+  await resolveTypeCommon({ node });
 
-    await resolveTypeCommon({ node });
-
-    resolveNextBlock({ node });
+  resolveNextBlock({ node });
 }
 
 module.exports = {
-    hits,
-    init,
-    resolve
+  hits,
+  init,
+  resolve
 }
