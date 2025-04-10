9f5c89ba11664b1a484e3619aa40d810d0d16ecf
gboyraz <gultekinboyraz@gmail.com>
2025-04-10 12:19:56 +0200

    chore: update version to 0.105.0, bump @fnet/shell-flow to 0.1.25, and update typescript to 5.8.3; simplify .d.ts creation process

--- src/builder/api/create-dts/index.js
+++ src/builder/api/create-dts/index.js
@@ -9,12 +9,15 @@ export default async function createDts({ atom, setProgress, context }) {
 
     await setProgress({ message: "Creating .d.ts" });
 
-    if(which('bun')) {
-      const result = await fnetShellJs(`bunx tsc`, { cwd: projectDir });
-      if (result.code !== 0) throw new Error('Couldnt create .d.ts files.');  
-    }
-    else{
-      const result = await fnetShellJs(`npx tsc`, { cwd: projectDir });
-      if (result.code !== 0) throw new Error('Couldnt create .d.ts files.');  
-    }
+    const result = await fnetShellJs(`tsc`, { cwd: projectDir });
+    if (result.code !== 0) throw new Error('Couldnt create .d.ts files.');  
+
+    // if(which('bun')) {
+    //   const result = await fnetShellJs(`bunx tsc`, { cwd: projectDir });
+    //   if (result.code !== 0) throw new Error('Couldnt create .d.ts files.');  
+    // }
+    // else{
+    //   const result = await fnetShellJs(`npx tsc`, { cwd: projectDir });
+    //   if (result.code !== 0) throw new Error('Couldnt create .d.ts files.');  
+    // }
 }
